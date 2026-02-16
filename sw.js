386e89b315df6846d3c9ba5c4d1533a2dfdc07e1
joshuee2 <joshuee@hanmail.net>
2026-02-16 18:56:43 +0900

    feat: build Android-friendly home workout coach app

--- sw.js
+++ sw.js
@@ -1,21 +1,24 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("lunch-menu-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/icon.png",
-        "/og_image.png",
-        "/manifest.json"
-      ]);
-    })
+const CACHE_NAME = "homeworkout-cache-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon.png",
+  "/og_image.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
 });
